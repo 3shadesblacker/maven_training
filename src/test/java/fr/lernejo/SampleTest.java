534951ae95158c88c9330796c88ddcd554cb9d4e
Mouhamad Mbaye <moustapha-mbaye@outlook.com>
2022-02-11 12:36:11 +0100

    Setup GitHub CI

--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -2,7 +2,7 @@ package fr.lernejo;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
 
 class SampleTest {
 
