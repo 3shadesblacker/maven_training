1541f9a6b918b991e456050c578e3191df4e5751
Mouhamad Mbaye <moustapha-mbaye@outlook.com>
2022-03-09 15:44:39 +0100

    Add test to match 100% coverage

--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -1,16 +1,4 @@
-package fr.lernejo;
-
-import org.junit.jupiter.api.Test;
-
-// import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.*;
 class SampleTest {
-
-    @Test
-    void op() {
-    }
-
-    @Test
-    void fact() {
-    }
+  
 }
