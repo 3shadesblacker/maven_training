e3fc5d6fd582b0712481d3f15af8bfd8f9208a74
Mouhamad Mbaye <moustapha-mbaye@outlook.com>
2022-03-09 15:55:55 +0100

    Add live badges

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # maven_training
-![workflow](https://github.com/deadfault/maven_training/actions/workflows/main.yml/badge.svg)
-![branch parameter](https://github.com/deadfault/maven_training/actions/workflows/main.yml/badge.svg?branch=feature-1)
-![event parameter](https://github.com/deadfault/maven_training/actions/workflows/main.yml/badge.svg?event=pull_request)
+[![workflow](https://github.com/3shadesblacker/maven_training/actions/workflows/main.yml/badge.svg)](https://github.com/3shadesblacker/maven_training/actions/workflows/build.yml)
+
+# codecov
+[![codecov](https://codecov.io/gh/3shadesblacker/maven_training/branch/main/graph/badge.svg)](https://codecov.io/gh/3shadesblacker/maven_training)
--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package fr.lernejo;
+
+//import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
+
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static fr.lernejo.Sample.Operation.ADD;
+import static fr.lernejo.Sample.Operation.MULT;
+
 class SampleTest {
-  
+
+    @Test
+    void op() {
+        Sample sa = new Sample();
+        Assertions.assertThat(sa.op(ADD, 5, 7)).isEqualTo(12);
+        Assertions.assertThat(sa.op(MULT, 5, 7)).isEqualTo(35);
+    }
+
+    @Test
+    void fact() {
+        Sample sa = new Sample();
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> sa.fact(-1));
+        Assertions.assertThat(sa.fact(5)).isEqualTo(120);
+    }
 }
